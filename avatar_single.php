8bb7418b4a213c43f66003ca29e7ab15816d6061
Leandro Menezes <lamenezes86@gmail.com>
2026-01-05 17:29:24 -0300

    Refactor avatar function to use get_avatar_data

--- avatar_single.php
+++ avatar_single.php
@@ -1,25 +1,29 @@
-add_filter('get_avatar', 'avatar_personalizado_id7', 1, 5);
-function avatar_personalizado_id7($avatar, $id_or_email, $size, $default, $alt) {
-    // Pega o ID do usuário (funciona para comentários e perfis)
+add_filter('get_avatar_data', 'avatar_fixo_alexandre', 100, 2);
+
+function avatar_fixo_alexandre($args, $id_or_email) {
+    $user_id = 0;
+
+    // Pega o ID do usuário de qualquer forma (post, comentário, etc.)
     if (is_numeric($id_or_email)) {
         $user_id = absint($id_or_email);
-    } elseif (is_object($id_or_email) && !empty($id_or_email->user_id)) {
-        $user_id = absint($id_or_email->user_id);
-    } elseif (is_object($id_or_email) && !empty($id_or_email->ID)) {
-        $user_id = absint($id_or_email->ID);
-    } else {
-        // Se não conseguir o ID, tenta pelo email
+    } elseif (is_object($id_or_email)) {
+        if (!empty($id_or_email->user_id)) {
+            $user_id = absint($id_or_email->user_id);
+        } elseif (!empty($id_or_email->ID)) {
+            $user_id = absint($id_or_email->ID);
+        }
+    } elseif (is_string($id_or_email)) {
         $user = get_user_by('email', $id_or_email);
-        $user_id = $user ? $user->ID : 0;
+        if ($user) $user_id = $user->ID;
     }
 
-    // Avatar apenas para o ID 7
-    if ($user_id == 7) {
-        $avatar_url = 'https://seusite.com/wp-content/uploads/2023/10/avatar-id7.jpg';
-        $avatar = '<img alt="' . esc_attr($alt) . '" src="' . esc_url($avatar_url) . '" class="avatar avatar-' . esc_attr($size) . ' photo" height="' . esc_attr($size) . '" width="' . esc_attr($size) . '" />';
+    if ($user_id === 7) {  // Troque 7 pelo ID do usuário desejado
+
+        $args['url'] = 'https://maismottoristas.com.br/wp-content/uploads/Alexandre.webp';
+        $args['found_avatar'] = true; // Força o WordPress a usar essa imagem
     }
 
-    return $avatar;
+    return $args;
 }
 
 /*
